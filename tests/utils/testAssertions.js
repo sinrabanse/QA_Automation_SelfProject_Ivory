35af70eb1d09e99b883a4230e41a77536d0442d0
Aleksandr Arkhipov <130854915+sinrabanse@users.noreply.github.com>
2025-04-10 18:31:56 +0300

    fix .gitignore

--- tests/utils/testAssertions.js
+++
@@ -1,5 +0,0 @@
-export const testAssertions = {
-  empty_cart_text:
-    "סל הקניות שלך ריק, שווה להרשם או להתחבר וליהנות מחוויית קנייה מעולה",
-  error_amount_product_text: "הרכישה באתר מוגבלת ל-20 יחידות ללקוח",
-};
--- tests/utils/testUIElements.js
+++
@@ -1,7 +0,0 @@
-export const testUIElements = {
-  iPhone16BlueName: "אייפון Apple iPhone 16 128GB בצבע כחול",
-  iPhone16BlackName: "אייפון Apple iPhone 16 128GB בצבע שחור",
-  iPhone16WhiteName: "אייפון Apple iPhone 16 128GB בצבע לבן",
-  termsOfUseCheckboxText:
-    "אני מאשר/ת כי קראתי, הבנתי והסכמתי לתנאי השימוש ולמדיניות הפרטיות *",
-};
